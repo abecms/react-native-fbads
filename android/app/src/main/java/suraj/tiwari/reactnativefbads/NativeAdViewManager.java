77b39b853d96aa043fc6c517687a04c90517bed2
Julien Jourde <julien.jourde@livingcolor.fr>
2020-04-17 20:11:07 +0200

    Manage annotations.

--- android/app/src/main/java/suraj/tiwari/reactnativefbads/NativeAdViewManager.java
+++ android/app/src/main/java/suraj/tiwari/reactnativefbads/NativeAdViewManager.java
@@ -1,7 +1,7 @@
 package suraj.tiwari.reactnativefbads;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 
